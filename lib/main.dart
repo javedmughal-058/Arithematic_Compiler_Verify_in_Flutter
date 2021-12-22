33352a093200426330b5e81943cd969078ad03c1
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-12-22 23:21:36 +0500

    update design code validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,120 @@
 import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget{
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    final textcontroller=TextEditingController();
-    final _formkey=GlobalKey();
-    var value;
-    Color hexToColor(String code) {
-      return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-    }
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Arithematic Verify ",
-        home: Material(
-            child: Form(
-              key: _formkey,
-              child: Container (
-                  padding: const EdgeInsets.all(30.0),
-                  color: Colors.white,
-                  child: Container(
-                    child: Center(
-                        child: Column(
-                            children : [
-                               Padding(padding: EdgeInsets.only(top: 140.0)),
-                               Text('Arithemtic Operator Verify',
-                                style: TextStyle(color: hexToColor("#F2A03D"), fontSize: 25.0),),
-                               Padding(padding: EdgeInsets.only(top: 50.0)),
-                               TextFormField(
-                                 controller: textcontroller,
-                                decoration: InputDecoration(
-                                  labelText: "Enter Operator ",
-                                  fillColor: Colors.white,
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(25.0),
-                                    borderSide: BorderSide(
-                                    ),
-                                  ),
-                                  //fillColor: Colors.green
-                                ),
-                                validator: (val) {
-                                  if(val?.length==0) {
-                                    return "Operator cannot be empty";
-                                  }else{
-                                    return null;
-                                  }
-                                },
-                                keyboardType: TextInputType.emailAddress,
-                                style: const TextStyle(
-                                  fontFamily: "Poppins",
-                                ),
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  bool _validate = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(backgroundColor: hexToColor("#F2A03D"),
+          title: Text('Compiler Project'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 9,
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      
+                      Text('Arithematic Operator Verify',style: TextStyle(
+                        color:  hexToColor("#F2A03D"),
+                        fontSize: 20,
+                      ),),
+                      SizedBox(height: 20,),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            errorText: _validate ? 'field Can\'t Be Empty' : null,
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter Operator',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 40,
+                            width: 120,
+                            child: RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  side: BorderSide(color: hexToColor("#F2A03D"))
                               ),
-                                SizedBox(height: 20,),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                   Container(
-                                      height: 40,
-                                      width: 140,
-                                      child: ElevatedButton(
-                                        style: ButtonStyle(
-                                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                            RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(18.0),
-                                              side: BorderSide(
-                                                color: hexToColor("#F2A03D"),
-                                                width: 2.0,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        child: Text('Verify'),
-                                        onPressed: () {
-                                          value=textcontroller.text;
-                                          if(value=='+' || value=='-' || value=='*' || value=='/' || value=='%'){
-                                            CoolAlert.show(
-                                              context: context,
-                                              type: CoolAlertType.success,
-                                              title: 'Successful',
-                                              text: "You entered Arithematic Operator $value",
-                                            );
-                                          }
-                                          else {
-                                            CoolAlert.show(
-                                              context: context,
-                                              type: CoolAlertType.error,
-                                              title: 'Error',
-                                              text: "invalid entry $value",
+                              textColor: Colors.white,
+                              color:  hexToColor("#F2A03D"),
+                              child: Text('Verify',style: TextStyle(fontSize: 16),),
+                              onPressed: (){
+                                if(nameController.text.toString()=='+' || nameController.text=='-' || nameController.text=='*' || nameController.text=='/' || nameController.text=='%'){
+                                  CoolAlert.show(
+                                    context: context,
+                                    title: 'Success',
+                                    type: CoolAlertType.success,
+                                    text: "You entered an operator ${nameController.text}",
+                                  );
+                                }
+                                else if (nameController.text.isEmpty){
+                                  setState(() {
+                                    nameController.text.isEmpty ? _validate = true : _validate = false;
+                                  });
+                                }
+                                else{
+                                  CoolAlert.show(
+                                    context: context,
+                                    title: 'Error',
+                                    type: CoolAlertType.error,
+                                    text: "invalid entry ${nameController.text}",
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                        ],
 
-                                            );
-                                          }
-                                        },
-                                      ),
-                                    ),
+                      ),
+                    ],
 
-
-                                ],
-
-                              )
-                            ]
-                        )
-                    ),
                   )
               ),
-            )
+            ),
+        Expanded(flex: 1,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text('developed by Muhammad Javed',style: TextStyle(
+              fontSize: 9,
+              color: hexToColor("#F2A03D"),
+            ),),
+          ],
+
+        ),),
+          ],
         )
+
+
     );
+  }
+
+  Color hexToColor(String code) {
+    return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
   }
 }
